aa5df8e9679aa82a7ac1d4d9be6c103ab08de154
Christoph <christoph.khouri@gmail.com>
2014-02-12 22:26:50 -0500

    Adding smtp

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 var MailParser = require("mailparser").MailParser;
 var mailparser = new MailParser();
+var simplesmtp = require("simplesmtp");
 
-var email = "From: 'Sender Name' <sender@example.com>\r\n"+
-            "To: 'Receiver Name' <receiver@example.com>\r\n"+
+var email = '';
+
+var test_email = "From: 'Sender' <sender@example.com>\r\n"+
+            "To: 'Receiver' <receiver@example.com>\r\n"+
             "Subject: Hello world!\r\n"+
             "\r\n"+
-            "How are you today?";
+            "Message";
 
-// setup an event listener when the parsing finishes
 mailparser.on("end", function(mail_object){
-    console.log("From:", mail_object.from); //[{address:'sender@example.com',name:'Sender Name'}]
-    console.log("Subject:", mail_object.subject); // Hello world!
-    console.log("Text body:", mail_object.text); // How are you today?
+	console.log(mail_object);
+    // console.log("From:", mail_object.from);
+    // console.log("Subject:", mail_object.subject);
+    // console.log("Text body:", mail_object.text);
 });
 
-// send the email source to the parser
-mailparser.write(email);
-mailparser.end();
+simplesmtp.createSimpleServer({
+		SMTPBanner: "NodemAIL",
+		ignoreTLS: true,
+		disableDNSValidation: true,
+		debug: false
+	}, function(req){
+		req.on("data", function(chunk){
+    		email += chunk;
+		});
+		req.on("end", function(){
+			mailparser.write(email);
+			mailparser.end();
+		});
+    	req.accept();
 
+}).listen(25, function(err){
+    if(!err){
+        console.log("SMTP server listening on port 25");
+    }else{
+        console.log("Could not start server on port 25. Ports under 1000 require root privileges.");
+        console.log(err.message);
+    }
+});
