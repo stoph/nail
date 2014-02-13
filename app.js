50f7fc7525f2c61b04c948cb6d0d1a81bb93f075
Christoph <christoph.khouri@gmail.com>
2014-02-13 00:29:59 -0500

    Adding request lib to POST message object

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var MailParser = require("mailparser").MailParser;
 var mailparser = new MailParser();
 var simplesmtp = require("simplesmtp");
+var request = require('request');
 
 var email = '';
 
 var test_email = "From: 'Sender' <sender@example.com>\r\n"+
             "To: 'Receiver' <receiver@example.com>\r\n"+
-            "Subject: Hello world!\r\n"+
+            "Subject: [AMP] Post title (12345)\r\n"+
             "\r\n"+
             "Message";
 
@@ -14,11 +15,16 @@ mailparser.on("end", function(mail_object){
 	console.log(mail_object);
     // console.log("From:", mail_object.from);
     // console.log("Subject:", mail_object.subject);
+    // console.log("headers:", mail_object.text);
     // console.log("Text body:", mail_object.text);
+	// console.log("HTML body:", mail_object.html);
+	
+	request.post('http://requestb.in/1gx0oxm1', {form: mail_object});
+	
 });
 
 simplesmtp.createSimpleServer({
-		SMTPBanner: "NodemAIL",
+		SMTPBanner: "Node Mail",
 		ignoreTLS: true,
 		disableDNSValidation: true,
 		debug: false
@@ -35,7 +41,7 @@ simplesmtp.createSimpleServer({
 }).listen(25, function(err){
     if(!err){
         console.log("SMTP server listening on port 25");
-    }else{
+    } else {
         console.log("Could not start server on port 25. Ports under 1000 require root privileges.");
         console.log(err.message);
     }
